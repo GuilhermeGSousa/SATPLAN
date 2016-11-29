a0dcc038960b9c9bce7a76a2c283cd386ce3577f
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2016-11-29 22:05:02 +0000

    DIMACS reading SAT Solver yet under development

--- SatSolver/SATTEST.py
+++ SatSolver/SATTEST.py
@@ -1,23 +1,9 @@
 import sys
-from GroundedLiteral import *
-from Clause import *
 from SatSolver import *
-
 def satPlan(argv):
-	g1=GroundedLiteral("x1",True)
-	g2=GroundedLiteral("x2",False)
-	g3=GroundedLiteral("x2",True)
+	cnf=CNF("input.dat")
 
-	clause1=Clause()
-	clause2=Clause()
-
-	clause1.addLiteral(g1)
-	clause1.addLiteral(g2)
-	clause2.addLiteral(g3)
-	
-	res = satSolver([clause1,clause2],[g1,g3])
-
-	print(res)
+	solveCNF(cnf.clauses,cnf.symbols)
 
 if __name__ == '__main__':
 	satPlan(sys.argv)
--- SatSolver/SatSolver.py
+++ SatSolver/SatSolver.py
@@ -1,2 +1,74 @@
-def satSolver():
-	pass
+from Cnf import *
+import copy
+
+
+
+
+def isEveryClauseTrue(clauses,model):
+	for c in clauses:
+		list_assigned=[l.name for l in c if l.name in model.var_sol.keys()]
+		clauseFalse=True
+		for l in list_assigned:
+			if model[l.name]==l.signal:
+				clauseFalse=False
+		if clauseFalse==True:
+			return False
+	return True
+
+def isAnyClauseFalse(clauses,model):
+	for c in clauses:
+		for l in c:
+			if not l.name in model.var_sol.keys():
+				return False
+			else:
+				clauseFalse=True
+				for l in list_assigned:
+					if model[l.name]==l.signal:
+						clauseFalse=False
+				if clauseFalse==True:
+					return True
+	return False				
+
+def isPureSymbol(clauses,symb):
+	for c in clauses:
+		pass
+
+def isUnitClause(clauses,symb):
+	for c in clauses:
+		pass
+
+
+def solveCNF(clauses,symbols,model=Solution()):
+
+	if isEveryClauseTrue(clauses,model):
+		print("All True")
+		return True 
+
+	if isAnyClauseFalse(clauses,model):
+		print("Any False")
+		return False
+
+	for i in range(0,len(symbols)):
+
+		res, val = isPureSymbol(clauses,symbols[i])
+		if res:
+			model[symbols.pop(i)]=val
+			return solveCNF(clauses,symbols,model)
+
+		res, val = isUnitClause(clauses,symbols[i])
+		if res:
+			model[symbols.pop(i)]=val
+			return solveCNF(clauses,symbols,model)
+
+	symb = symbols.pop()
+	rest = symbols
+
+	rest_copy=copy.deepcopy(rest)
+	model_copy=copy.deepcopy(model)
+
+	model[symb]=True
+	model_copy[symb]=False
+
+
+	return solveCNF(clauses,rest,model) or solveCNF(clauses,rest_copy,model_copy)
+
